355ccaab678329ce2ee5c029672d6256c9308b7b
samc5 <scowan30@stuy.edu>
2022-09-29 21:47:34 -0400

    added classwork

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,26 +1,11 @@
 """ 
-David Chen, Diana Akhmedova, Sam Cowan
+Jacob Guo, Sam Cowan
 SoftDev
-04_choose
-2022-09-22
+05_bitstream
+2022-09-28
 time spent: 0.5 hours
 
-DISCO: 
- - random.choice returns a random element from given sequence.
- - A dictionary stores keys and values and allows us to reference them by key.
 
-QCC:
- - Are these pseudorandom?
- - What does pseudorandom numbers entail?
-
-OPS SUMMARY:
- 1. Given krewes = {dict(int : list)}, we randomly chose a period (2, 7, or 8) using random.choice,
-    and printed the random period.
- 2. We retreived the corresponding list from the randomly selected period
-    by setting it to a temporary variable called "data."
- 3. We returned a randomly selected Devo by using random.randint.
- 4. We also created another function that utilizes random.random and floors the output,
-    which essentially achieves the same outcome.
 """
 
 import random
@@ -36,6 +21,7 @@ krewes = {
            7: ["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
            8: ["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
          }
+
 krewesNew = {
             2: {},
             7: {},
@@ -65,9 +51,30 @@ def chooseDevoWithRandom():
     data = krewes[randomPd]
     return data[math.floor(random.random() * len(data))]
 
-print("RANDOM.RANDINT:")
-print("Random Devo: ", chooseDevoWithInt())
-print("\nRANDOM.CHOICE:")
-print("Random Devo: ", chooseDevoWithChoice())
-print("\nRANDOM.RANDOM:")
-print("Random Devo: ", chooseDevoWithRandom())
+def split_krewes():
+    krewes_file = open("krewes.txt", "r")
+    read_file = krewes_file.read()
+    krewes_truple = read_file.split("@@@")
+    for i in range(len(krewes_truple)):
+        double_split = krewes_truple[i].split("$$$")
+        krewesNew[int(double_split[0])].update({double_split[1]: double_split[2]})
+    pd = chooseRandomPd()
+    dict_index = math.floor(random.random() * len(krewesNew[pd]))
+    print(pd)
+    keys = krewesNew[pd].keys()
+    print(keys)
+
+
+
+
+
+# print("RANDOM.RANDINT:")
+# print("Random Devo: ", chooseDevoWithInt())
+# print("\nRANDOM.CHOICE:")
+# print("Random Devo: ", chooseDevoWithChoice())
+# print("\nRANDOM.RANDOM:")
+# print("Random Devo: ", chooseDevoWithRandom())
+
+# print(read_file)
+# print(krewes_truple)
+print(split_krewes())
