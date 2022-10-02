7f1c26f36b370e4cde22580e9c5863f5befc4951
samc5 <scowan30@stuy.edu>
2022-10-02 15:09:12 -0400

    slight update to 06

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,19 +1,32 @@
 import random
 import csv
+"""
 
 
-occupations = open("occupations.csv","r")
-reader = csv.reader(occupations, delimiter = ',')
-
-oc_dict = {}
-for row in reader:
-    if row[1] != "Percentage":
-        if row[0] != "Total":
-            oc_dict.update({row[0] : float(row[1])})
 
 
-keys = list(oc_dict.keys())
-values = list(oc_dict.values())
+"""
 
-random_occupation = random.choices(population = keys,weights = values, k = 1)
-print(random_occupation)
+
+def rand_occupation():
+    occupations = open("occupations.csv","r")
+    reader = csv.reader(occupations, delimiter = ',')
+
+    oc_dict = {}
+    for row in reader:
+        if row[1] != "Percentage":
+            if row[0] != "Total":
+                oc_dict.update({row[0] : float(row[1])})
+
+
+    keys = list(oc_dict.keys())
+    values = list(oc_dict.values())
+
+    random_occupation = random.choices(population = keys,weights = values, k = 1)
+    return random_occupation
+
+count = 0
+for i in range(1000):
+    if rand_occupation()[0] == "Farming, fishing and forestry":
+        count += 1
+print(count)
